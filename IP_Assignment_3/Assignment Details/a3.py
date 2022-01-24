5d3d2b82d77c29541f0a5555c1a22fe8c76913df
HARJEET <harjeet20561@iiitd.ac.in>
2022-01-24 18:55:10 +0530

    Assignment-3

---
+++ IP_Assignment_3/Assignment Details/a3.py
@@ -0,0 +1,176 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# NO other imports are allowed
+
+class Shape:
+    '''
+    DO NOT MODIFY THIS CLASS
+
+    DO NOT ADD ANY NEW METHODS TO THIS CLASS
+    '''
+    def __init__(self):
+        self.T_s = None
+        self.T_r = None
+        self.T_t = None
+    
+    
+    def translate(self, dx, dy):
+        '''
+        Polygon and Circle class should use this function to calculate the translation
+        '''
+        self.T_t = np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+ 
+
+    def scale(self, sx, sy):
+        '''
+        Polygon and Circle class should use this function to calculate the scaling
+        '''
+        self.T_s = np.array([[sx, 0, 0], [0, sy, 0], [0, 0, 1]])
+ 
+        
+    def rotate(self, deg):
+        '''
+        Polygon and Circle class should use this function to calculate the rotation
+        '''
+        rad = deg*(np.pi/180)
+        self.T_r = np.array([[np.cos(rad), np.sin(rad), 0],[-np.sin(rad), np.cos(rad),0], [0, 0, 1]])
+
+        
+    def plot(self, x_dim, y_dim):
+        '''
+        Polygon and Circle class should use this function while plotting
+        x_dim and y_dim should be such that both the figures are visible inside the plot
+        '''
+        x_dim, y_dim = 1.2*x_dim, 1.2*y_dim
+        plt.plot((-x_dim, x_dim),[0,0],'k-')
+        plt.plot([0,0],(-y_dim, y_dim),'k-')
+        plt.xlim(-x_dim,x_dim)
+        plt.ylim(-y_dim,y_dim)
+        plt.grid()
+        plt.show()
+
+
+
+class Polygon(Shape):
+    '''
+    Object of class Polygon should be created when shape type is 'polygon'
+    '''
+    def __init__(self, A):
+        '''
+        Initializations here
+        '''
+        pass
+ 
+    
+    def translate(self, dx, dy):
+        '''
+        Function to translate the polygon
+    
+        This function takes 2 arguments: dx and dy
+    
+        This function returns the final coordinates
+        '''
+        pass
+
+    
+    def scale(self, sx, sy):
+        '''
+        Function to scale the polygon
+    
+        This function takes 2 arguments: sx and sx
+    
+        This function returns the final coordinates
+        '''
+        pass
+ 
+    
+    def rotate(self, deg, rx = 0, ry = 0):
+        '''
+        Function to rotate the polygon
+    
+        This function takes 3 arguments: deg, rx(optional), ry(optional). Default rx and ry = 0. (Rotate about origin)
+    
+        This function returns the final coordinates
+        '''
+        pass
+    
+
+    def plot(self):
+        '''
+        Function to plot the polygon
+    
+        This function should plot both the initial and the transformed polygon
+    
+        This function should use the parent's class plot method as well
+    
+        This function does not take any input
+    
+        This function does not return anything
+        '''
+        pass
+
+
+class Circle(Shape):
+    '''
+    Object of class Circle should be created when shape type is 'circle'
+    '''
+    def __init__(self, x=0, y=0, radius=5):
+        '''
+        Initializations here
+        '''
+        pass
+
+    
+    def translate(self, dx, dy):
+        '''
+        Function to translate the circle
+    
+        This function takes 2 arguments: dx and dy (dy is optional).
+    
+        This function returns the final coordinates and the radius
+        '''
+        pass
+ 
+        
+    def scale(self, sx):
+        '''
+        Function to scale the circle
+    
+        This function takes 1 argument: sx
+    
+        This function returns the final coordinates and the radius
+        '''
+        pass
+ 
+    
+    def rotate(self, deg, rx = 0, ry = 0):
+        '''
+        Function to rotate the circle
+    
+        This function takes 3 arguments: deg, rx(optional), ry(optional). Default rx and ry = 0. (Rotate about origin)
+    
+        This function returns the final coordinates and the radius
+        '''
+        pass
+ 
+    
+    def plot(self):
+        '''
+        Function to plot the circle
+    
+        This function should plot both the initial and the transformed circle
+    
+        This function should use the parent's class plot method as well
+    
+        This function does not take any input
+    
+        This function does not return anything
+        '''
+        pass
+        
+
+if __name__ == "__main__":
+    '''
+    Add menu here as mentioned in the sample output section of the assignment document.
+    '''
+    pass
